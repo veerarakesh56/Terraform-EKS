ca427a8acae9dac3447a994e0d246e8843d046b9
Veera Rakesh Kethari <veerarakesh56@gmail.com>
2024-07-14 20:14:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "aws_region" {
+  default = "us-west-1"
+  description = "aws region"
+}
