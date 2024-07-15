274728474be0aff9367b39e273bf64e3f7aa5bd1
Veera Rakesh Kethari <veerarakesh56@gmail.com>
2024-07-15 22:07:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "kubernetes_version" {
+  default     = 1.28
+  description = "kubernetes version"
+}
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "default CIDR range of the VPC"
